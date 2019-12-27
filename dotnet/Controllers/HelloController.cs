e9a9e3570559e1d73fb99a20f18b29e186280829
Marco Alfaro <marco.alfaro@gmail.com>
2019-12-27 16:52:09 -0600

    first commit

---
+++ dotnet/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {   
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello to my friends!";
+        }
+    }
+}
